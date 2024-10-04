a1f810b09bc38039f6a64a751fc46d72736ec862
Soumil24 <soumilking45@gmail.com>
2024-10-05 00:34:54 +0530

    28 fix

--- 28_Digital_Clock/script.js
+++ 28_Digital_Clock/script.js
@@ -1,21 +1,11 @@
 let hour = document.querySelector("#hour")
 let min = document.querySelector("#min")
 let sec = document.querySelector("#sec")
+setInterval(() => {
 
 let date = new Date()
 
-if (date.getHours() < 10) {
-    hour.innerHTML = "0" +  date.getHours()
-} else {
-    hour.innerHTML = date.getHours()
-}
-if (date.getMinutes() < 10) {
-    min.innerHTML = "0" + date.getMinutes()
-} else {
-    min.innerHTML = date.getMinutes()
-}
-if (date.getSeconds() < 10) {
-    sec.innerHTML = "0" + date.getSeconds()
-} else {
-    sec.innerHTML = date.getSeconds()
-}
+    hour.innerHTML = (date.getHours()<10?"0":"") + date.getHours()
+    min.innerHTML = (date.getMinutes()<10?"0":"") + date.getMinutes()
+    sec.innerHTML = (date.getSeconds()<10?"0":"") + date.getSeconds()
+}, 1000);
